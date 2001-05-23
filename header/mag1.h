eee3473b444a7e9bc7b3628b0614805af347bed8
Bob Biedron <Robert.T.Biedron@nasa.gov>
2001-05-23 20:12:23 +0000

    Initial revision

---
+++ header/mag1.h
@@ -0,0 +1,24 @@
+      parameter(mdim=14,jdim=121,kdim=45,ldim=2,mhldim=2,
+     .          ibdim=2000,idim=8000,ndim=200,ipmax=28,
+     .          iwrdim=idim+ibdim,mlen=jdim*kdim*ldim)
+c
+c     parameter definitions:
+c
+c     mdim................max number of input grids
+c
+c     jdim,kdim,ldim......max j, k, l dimensions of any grid 
+c
+c     mholdim.............max number of holes/outer boundaries (per grid)   
+c
+c     idim................max number of fringe points (total over all grids)
+c                         (also max number of boundary points)
+c 
+c     ibdim...............max number of points allowed for storage of 
+c                         the initial hole hole boundary (per hole)
+c    
+c     ndim................largest dimension of any coordinate surface used
+c                         for hole cutting
+c
+c     ipmax...............max number of coordinate surfaces used for cutting
+c                         holes (total over all grids) 
+c
---
+++ source/cfl3d/libs/bessel.c
@@ -0,0 +1,62 @@
+#include <math.h>
+
+
+void j0_(double *x, double *ans)
+{
+  *ans = j0(*x);
+}
+
+void j1_(double *x, double *ans)
+{
+  *ans = j1(*x);
+}
+
+void jn_(int *n, double *x, double *ans)
+{
+  *ans = jn(*n,*x);
+}
+
+void y0_(double *x, double *ans)
+{
+  *ans = y0(*x);
+}
+
+void y1_(double *x, double *ans)
+{
+  *ans = y1(*x);
+}
+
+void yn_(int *n, double *x, double *ans)
+{
+  *ans = yn(*n,*x);
+}
+
+void j0sp_(float *x, float *ans)
+{
+  *ans = j0((double)*x);
+}
+
+void j1sp_(float *x, float *ans)
+{
+  *ans = j1(*x);
+}
+
+void jnsp_(int *n, float *x, float *ans)
+{
+  *ans = jn(*n,*x);
+}
+
+void y0sp_(float *x, float *ans)
+{
+  *ans = y0(*x);
+}
+
+void y1sp_(float *x, float *ans)
+{
+  *ans = y1(*x);
+}
+
+void ynsp_(int *n, float *x, float *ans)
+{
+  *ans = yn(*n,*x);
+}
